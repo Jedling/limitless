9eee3ec88dcffe6e831ef527b5d5f3519d324ca5
edling <joel.edling1@gmail.com>
2020-01-20 11:34:05 +0100

    Model property added

--- src/types/MenuModel.ts
+++ src/types/MenuModel.ts
@@ -1,12 +1,14 @@
 interface IMenuModel {
   Id: number;
   Name: string;
+  LoggedInName: string;
   Path: string;
 }
 
 export class MenuModel implements IMenuModel {
   public Id: number;
   public Name: string;
+  public LoggedInName: string;
   public Path: string;
   /**
    *
@@ -14,10 +16,12 @@ export class MenuModel implements IMenuModel {
   constructor(
     id: number = 0,
     name: string = '',
+    loggedInName: string = '',
     path: string = '',
   ) {
     this.Id = id;
     this.Name = name;
+    this.LoggedInName = loggedInName;
     this.Path = path;
   }
 }
