cb5939814afc3a3d418d9fb41a9fba6c73137e65
edling <joel.edling1@gmail.com>
2020-01-20 17:20:51 +0100

    removed stuff

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -27,10 +27,8 @@ export default class AboutUs extends Vue {}
     box-shadow: 0 1px 2px rgba(0, 0, 0, 0.7);
     @media only screen and (max-width: 568px) {
       font-size: 2em;
-      // margin-bottom: 10px;
     }
   }
-
 }
 </style>
 
