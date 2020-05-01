31a6574fca86f645fcf45753d7efd3bf0e8045bf
jedling <joel.edling1@gmail.com>
2020-05-02 01:30:38 +0200

    new component added

---
+++ src/components/KfumLogo.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="wrapper">
+    <figure>
+      <img class="kfum-logo" :src="kfumLogo" alt="Logotyp Kfum" />
+    </figure>
+  </section>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class KfumLogo extends Vue {
+  private kfumLogo: string = "/img/kfum.png";
+}
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
+  .kfum-logo {
+    width: 20%;
+    @media only screen and (max-width: 768px) {
+      width: 30%;
+    }
+  }
+}
+</style>
