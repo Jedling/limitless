035677c735b5fde17ad0b06b72f5ba7c755b5550
jedling <joel.edling1@gmail.com>
2020-05-02 01:31:36 +0200

    data added to schedule entity

--- src/assets/data/TempSchedule.ts
+++ src/assets/data/TempSchedule.ts
@@ -1,24 +1,30 @@
 import { ScheduleModel } from "@/types/ScheduleModel";
 
 const tempScheduleData: ScheduleModel[] = [
+  // {
+  //   Id: 1,
+  //   Weekday: "MÅNDAG",
+  //   Activities: [
+  //     {
+  //       Id: 1,
+  //       Name: "BOXNING",
+  //       Location: "TALLRIKEN, PILDAMMSPARKEN",
+  //       Time: "17:30 - 18:30"
+  //     }
+  //   ]
+  // },
   {
-    Id: 1,
+    Id: 2,
     Weekday: "TISDAG",
     Activities: [
       {
         Id: 1,
-        Name: "LIMITLESS HIIT IN THE PARK",
-        Location: "TALLRIKEN, PILDAMMSPARKEN",
-        Time: "17:30 - 18:30"
-      }
-    ]
-  },
-  {
-    Id: 2,
-    Weekday: "TORSDAG",
-    Activities: [
+        Name: "LIMITLESS HIIT FLOW YOGA",
+        Location: "GRÄSMATTAN BREDVID KFUM-HUSET",
+        Time: "12:00 - 12:45"
+      },
       {
-        Id: 1,
+        Id: 2,
         Name: "LIMITLESS HIIT IN THE PARK",
         Location: "TALLRIKEN, PILDAMMSPARKEN",
         Time: "17:30 - 18:30"
@@ -27,6 +33,48 @@ const tempScheduleData: ScheduleModel[] = [
   },
   {
     Id: 3,
+    Weekday: "ONSDAGAR",
+    Activities: [
+      {
+        Id: 1,
+        Name: "KICK/THAIBOXNING",
+        Location: "TALLRIKEN, PILDAMMSPARKEN",
+        Time: "17:30 - 18:30"
+      }
+    ]
+  },
+  {
+    Id: 4,
+    Weekday: "TORSDAG",
+    Activities: [
+      {
+        Id: 1,
+        Name: "LIMITLESS ENERGY",
+        Location: "GRÄSMATTAN BREDVID KFUM-HUSET",
+        Time: "12:00 - 12:45"
+      },
+      {
+        Id: 2,
+        Name: "LIMITLESS HIIT IN THE PARK",
+        Location: "TALLRIKEN, PILDAMMSPARKEN",
+        Time: "17:30 - 18:30"
+      }
+    ]
+  },
+  // {
+  //   Id: 5,
+  //   Weekday: "FREDAG",
+  //   Activities: [
+  //     {
+  //       Id: 1,
+  //       Name: "KICK/THAIBOXNING samt BOXNING",
+  //       Location: "TALLRIKEN, PILDAMMSPARKEN",
+  //       Time: "17:30 - 18:30"
+  //     }
+  //   ]
+  // },
+  {
+    Id: 6,
     Weekday: "LÖRDAG",
     Activities: [
       {
@@ -34,19 +82,31 @@ const tempScheduleData: ScheduleModel[] = [
         Name: "LIMITLESS HIIT IN THE PARK",
         Location: "PILDAMMSTEATERN",
         Time: "10:30 - 11:30"
+      },
+      {
+        Id: 2,
+        Name: "URBAN HIIT FLOW YOGA",
+        Location: "PILDAMMSTEATERN",
+        Time: "11.30-12.30,"
       }
     ]
   },
   {
-    Id: 4,
+    Id: 7,
     Weekday: "SÖNDAG",
     Activities: [
+      // {
+      //   Id: 1,
+      //   Name: "HIKARI DO",
+      //   Location: "PILDAMMSTEATERN",
+      //   Time: "11:0 - 12:00"
+      // },
       {
-        Id: 1,
+        Id: 2,
         Name: "LIMITLESS HIIT IN THE PARK",
         Location: "PILDAMMSTEATERN",
         Time: "12:00 - 13:00"
-      }
+      },
     ]
   }
 ];
