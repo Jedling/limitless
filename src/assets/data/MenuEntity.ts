49c068fd519d1cd3769a87a9cb4e59d2b81ea36a
jedling <joel.edling1@gmail.com>
2020-05-02 01:31:52 +0200

    covid added to menu,

--- src/assets/data/MenuEntity.ts
+++ src/assets/data/MenuEntity.ts
@@ -1,75 +1,78 @@
-import { MenuModel } from '@/types/MenuModel';
+import { MenuModel } from "@/types/MenuModel";
 
 const menuData: MenuModel[] = [
-
   {
     Id: 1,
-    Name: 'OM FÖRENINGEN',
-    LoggedInName: 'OM FÖRENINGEN',
-    Path: '/om-limitless',
+    Name: "OM FÖRENINGEN",
+    LoggedInName: "OM FÖRENINGEN",
+    Path: "/om-limitless"
   },
   {
     Id: 2,
-    Name: 'SCHEMA',
-    LoggedInName: 'SCHEMA',
-    Path: '/schema',
+    Name: "SCHEMA",
+    LoggedInName: "SCHEMA",
+    Path: "/schema"
   },
   {
     Id: 3,
-    Name: 'KONTAKTA OSS',
-    LoggedInName: 'HITTA -OCH KONTAKTA OSS',
-    Path: 'hitta-oss',
+    Name: "KONTAKTA OSS",
+    LoggedInName: "HITTA -OCH KONTAKTA OSS",
+    Path: "hitta-oss"
   },
   {
     Id: 4,
-    Name: 'TRÄNINGSPASS',
-    LoggedInName: 'TRÄNINGSPASS',
-    Path: '/traningspass'
+    Name: "TRÄNINGSPASS",
+    LoggedInName: "TRÄNINGSPASS",
+    Path: "/traningspass"
   },
   {
     Id: 5,
-    Name: 'HIKARI DO',
-    LoggedInName: 'HIKARI DO',
-    Path: '/hikari-do',
+    Name: "HIKARI DO",
+    LoggedInName: "HIKARI DO",
+    Path: "/hikari-do"
   },
   {
     Id: 6,
-    Name: 'PROJEKT',
-    LoggedInName: 'PROJEKT',
-    Path: '/projekt',
+    Name: "PROJEKT",
+    LoggedInName: "PROJEKT",
+    Path: "/projekt"
   },
-  {
-    Id: 7,
-    Name: 'IT-POLICY / GDPR',
-    LoggedInName: 'IT-POLICY / GDPR',
-    Path: '/it-policies',
-  },
+  // {
+  //   Id: 7,
+  //   Name: "IT-POLICY / GDPR",
+  //   LoggedInName: "IT-POLICY / GDPR",
+  //   Path: "/it-policies"
+  // },
   {
     Id: 8,
-    Name: 'PRISER',
-    LoggedInName: 'PRISER',
-    Path: '/priser',
+    Name: "PRISER",
+    LoggedInName: "PRISER",
+    Path: "/priser"
   },
-  {
-    Id: 9,
-    Name: 'PARTNERS',
-    LoggedInName: 'PARTNERS',
-    Path: '/partners',
-  },
-  {
-    Id: 10,
-    Name: 'LOGGA IN',
-    LoggedInName: 'MITT KONTO',
-    Path: '/inloggning',
-  },
+  // {
+  //   Id: 9,
+  //   Name: 'PARTNERS',
+  //   LoggedInName: 'PARTNERS',
+  //   Path: '/partners',
+  // },
+  // {
+  //   Id: 10,
+  //   Name: 'LOGGA IN',
+  //   LoggedInName: 'MITT KONTO',
+  //   Path: '/inloggning',
+  // },
   {
     Id: 11,
-    Name: 'NY DELTAGARE / PROVA PÅ',
-    LoggedInName: 'NY DELTAGARE / PROVA PÅ',
-    Path: '/ny-deltagare',
+    Name: "NY DELTAGARE / PROVA PÅ",
+    LoggedInName: "NY DELTAGARE / PROVA PÅ",
+    Path: "/ny-deltagare"
   },
-
- 
+  {
+    Id: 12,
+    Name: "COVID-19",
+    LoggedInName: "COVID-19",
+    Path: "/covid-19"
+  }
 ];
 const menuType: MenuModel[] = menuData;
 
