0469e2fd68c00af18e2fe9e7d7fced20a6815df9
jedling <joel.edling1@gmail.com>
2020-05-02 01:32:16 +0200

    formatting

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,23 @@
   <div id="app">
     <Header />
     <Main>
-    <router-view />
+      <router-view />
     </Main>
     <TempPopUpModal />
     <Footer />
   </div>
 </template>
- <script lang="ts">
+<script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
-import TempPopUpModal from '@/components/TempPopUpModal.vue'
+import TempPopUpModal from "@/components/TempPopUpModal.vue";
 
 @Component({
   components: {
     Header,
     Footer,
-        TempPopUpModal
+    TempPopUpModal
   }
 })
 export default class App extends Vue {}
@@ -39,13 +39,13 @@ main {
 }
 #app {
   --primary: #55585f;
-  --secondary: #ADD8F9;
+  --secondary: #add8f9;
   --menuText: #fdfdfd;
   --hover: #7d8080;
 
   // font-family: "Avenir", Helvetica, Arial, sans-serif;
   // font-family: -apple-system,BlinkMacSystemFont,segoe ui,Roboto ,Oxygen-Sans,Ubuntu,Cantarell,helvetica neue,sans-serif;
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -54,6 +54,5 @@ main {
   min-height: 100vh;
   flex-direction: column;
   margin: 0;
-  
 }
 </style>
